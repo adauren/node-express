b67cda92db89c670ebe5167e885dc2ac15cce315
adauren <frankcawperwood818@gmail.com>
2019-07-06 16:59:14 +0600

    node-express app

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const homeRoutes = require("./routes/home");
-const coursesRoutes = require("./routes/courses");
+const cardRoutes = require("./routes/card");
 const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
 
 const app = express();
 
@@ -16,14 +17,16 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/", homeRoutes);
-app.use("/courses", coursesRoutes);
 app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/card", cardRoutes);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = Router();
 
 router.get("/", (req, res) => {
   res.render("index", {
-    title: "Home",
+    title: "Главная страница",
     isHome: true
   });
 });
